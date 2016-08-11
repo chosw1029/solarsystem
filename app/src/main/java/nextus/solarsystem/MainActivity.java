e0cd05c151b2abca16124e6233495f94cca34924
ReStartAllKill_note <chosw10293@naver.com>
2016-08-11 17:19:49 +0900

    solarsystem

---
+++ app/src/main/java/nextus/solarsystem/MainActivity.java
@@ -0,0 +1,206 @@
+package nextus.solarsystem;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.kakao.auth.ApiResponseCallback;
+import com.kakao.auth.ErrorCode;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.helper.log.Logger;
+
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView user_id;
+    CircleImageView userImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestMe();
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        user_id = (TextView) findViewById(R.id.user_info);
+
+
+        View nav_header = navigationView.getHeaderView(0);
+
+        userImg = (CircleImageView) nav_header.findViewById(R.id.user_img);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    protected void showSignup() {
+        setContentView(R.layout.layout_usermgmt_signup);
+        final ExtraUserPropertyLayout extraUserPropertyLayout = (ExtraUserPropertyLayout) findViewById(R.id.extra_user_property);
+        Button signupButton = (Button) findViewById(R.id.buttonSignup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                requestSignUp(extraUserPropertyLayout.getProperties());
+            }
+        });
+    }
+
+    private void requestSignUp(final Map<String, String> properties) {
+        UserManagement.requestSignup(new ApiResponseCallback<Long>() {
+            @Override
+            public void onNotSignedUp() {
+            }
+
+            @Override
+            public void onSuccess(Long result) {
+                requestMe();
+            }
+
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                final String message = "UsermgmtResponseCallback : failure : " + errorResult;
+                com.kakao.util.helper.log.Logger.w(message);
+                KakaoToast.makeToast(self, message, Toast.LENGTH_LONG).show();
+                finish();
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+            }
+        }, properties);
+    }
+
+    /**
+     * 사용자의 상태를 알아 보기 위해 me API 호출을 한다.
+     */
+    protected void requestMe() {
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String message = "failed to get user info. msg=" + errorResult;
+                Logger.d(message);
+
+                ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                    KakaoToast.makeToast(getApplicationContext(), getString(R.string.error_message_for_service_unavailable), Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    redirectLoginActivity();
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                redirectLoginActivity();
+            }
+
+            @Override
+            public void onSuccess(UserProfile userProfile) {
+                Log.e("UserProfile : " + userProfile, "");
+                Logger.d("UserProfile : " + userProfile);
+                GlobalApplication.getGlobalApplicationContext().setUserProfile(userProfile);
+                user_id.setText(""+userProfile.getNickname());
+                Glide.with(getApplicationContext()).load(userProfile.getProfileImagePath()).centerCrop().into(userImg);
+            }
+
+            @Override
+            public void onNotSignedUp() {
+                showSignup();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/nextus/solarsystem/SplashActivity.java
@@ -0,0 +1,25 @@
+package nextus.solarsystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by leoshin on 15. 6. 18..
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash);
+
+        findViewById(R.id.splash).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, SampleLoginActivity.class));
+                finish();
+            }
+        }, 500);
+    }
+}
